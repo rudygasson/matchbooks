4bbbdfb40bbaab5c855a69d61f2c569bbc606d3b
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2022-05-31 17:50:13 +0200

    2 create location and meeting models and migrations (#4)
    * Create location, meeting and user_location models
    * Fix enum type problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :locations, through: :user_locations
   has_many :copies
 end
