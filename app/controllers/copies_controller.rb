e13f205e177ff98c6649b046438c19e7fdef7c28
Philipp Riedel <riedel.philipp@gmx.net>
2022-06-01 12:50:43 +0200

    add destroy action and get rid of simple form attributes

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -11,54 +11,62 @@ class CopiesController < ApplicationController
   end
 
   def create
-    isbn = params[:book][:isbn]
+    # book_params
+    isbn = book_params[:isbn]
     # check if the isbn already exists in the DB
-    book = Book.find_by isbn: isbn
-    # if no isbn matches => add the book to books and copies table
-    if book.nil?
-      # create new Book instance
-      book = Book.new(book_params)
-      # get json from Google Books API
-      url = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}&maxResults=1"
-      response = HTTParty.get(url)
-      # convert response to JSON
-      result = response.parsed_response
+    @book = Book.find_by isbn: isbn
+    # if no isbn matches => get book info from google books API
+    if @book.nil?
+      @book = make_google_books_request(book_params, isbn)
+    end
+    # create copy
+    @copy = Copy.new
+    @copy.user = current_user
+    @copy.book = @book
+    if @copy.save
+      # redirect to user library
+      redirect_to copies_path
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  def destroy
+    @copy = Copy.find(params[:id])
+    @copy.destroy
+    # redirect to user library
+    redirect_to copies_path, status: :see_other
+  end
+
+  private
+
+  def make_google_books_request(book_params, isbn)
+    # create new Book instance
+    @book = Book.new(book_params)
+    # get json from Google Books API
+    url = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}&maxResults=1"
+    response = HTTParty.get(url)
+    # convert response to JSON
+    result = response.parsed_response
+    # if no isbn can be found
+    if result['totalItems'] > 0
       # extract book parameters
-      book.title = result["items"][0]["volumeInfo"]["title"]
-      book.author = result["items"][0]["volumeInfo"]["authors"][0]
-      book.description = result["items"][0]["volumeInfo"]["description"]
-      book.year = result["items"][0]["volumeInfo"]["publishedDate"].slice(0, 4).to_i
-      book.thumbnail = result["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+      @book.title = result["items"][0]["volumeInfo"]["title"]
+      @book.author = result["items"][0]["volumeInfo"]["authors"][0]
+      @book.description = result["items"][0]["volumeInfo"]["description"]
+      @book.year = result["items"][0]["volumeInfo"]["publishedDate"].slice(0, 4).to_i
+      @book.thumbnail = result["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
       id = result["items"][0]["id"]
       book.cover_image = "https://books.google.com/books/content/images/frontcover/#{id}?fife=w480-h960"
       # clarify if we want to add genre / category
       # book.category = result["items"][0]["volumeInfo"]["categories"][0]
-      # create copy
-      @copy = Copy.new
-      @copy.user = current_user
-      @copy.book = book
-      if @copy.save
-        # redirect to user library
-        redirect_to copies_path
-      else
-        render(:new, status: :unprocessable_entity)
-      end
-    # if isbn matches => add the book to copies table
-    else
-      # create copy
-      @copy = Copy.new
-      @copy.user = current_user
-      @copy.book = book
-      if @copy.save
-        # redirect to user library
-        redirect_to copies_path
-      else
-        render(:new, status: :unprocessable_entity)
-      end
+      book
+    # else
+    #   # redirect_to new_copy_path, status: :see_other
+    #   render(:new, status: :unprocessable_entity)
+    #   # raise
     end
   end
-
-  private
 
   def book_params
     params.require(:book).permit(
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :copies, only: %i[index new create]
+  resources :copies, only: %i[index new create destroy]
 end
