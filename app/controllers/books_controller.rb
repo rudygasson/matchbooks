205aa582a1bd3fe3b337553861bec2e3797064f9
Philipp Riedel <riedel.philipp@gmx.net>
2022-06-02 14:49:32 +0200

    add copy_id via link_to to the params of the linked path

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,16 +10,9 @@ class BooksController < ApplicationController
     # match book users with selected district, e. g. "Neukölln"
     @locations = []
     @book_users.each do |book_user|
-      @locations += book_user.locations.select { |location| location.district == @search_district}
+      @locations += book_user.locations.select { |location| location.district == @search_district }
     end
     # get distinct list of locations where users are willing to meet
     @locations_uniq = @locations.map { |location| location }.uniq
-    # get distinct list of users that have the book and are willing to meet at the locations
-    # @book_users_at_location = @locations_uniq.map { |location| location.users }
-    # raise
-
-    ###### params to parse #####
-    # 1) copy_id
-    # 2) location
   end
 end
