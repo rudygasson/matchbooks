0058be8ac6e3400de3f5e8412c52381cc1d5e15c
Soma Zsoter <somazsoter@gmail.com>
2022-06-02 14:58:01 +0200

    fix selection bug, extend search functionality with empty fields, update all the coresponding view texts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,15 @@ class PagesController < ApplicationController
     @search_district = params[:district]
     @search_term = params[:query]
     sql_query = "title ILIKE :query OR author ILIKE :query"
-    if @search_term
+    if (@search_term != nil && @search_term != "") && @search_district != "All areas"
       @filtered_by_query = Book.where(sql_query, query: "%#{@search_term}%")
       @filtered_books = @filtered_by_query.select { |book| book_districts(book).include?(@search_district.downcase) }
+    elsif (@search_term != nil && @search_term != "") && @search_district == "All areas"
+      @filtered_books = Book.where(sql_query, query: "%#{@search_term}%")
+    elsif @search_term == "" && @search_district != "All areas"
+      @filtered_books = Book.all.select { |book| book_districts(book).include?(@search_district.downcase) }
     else
-      @filtered_books = Book.all.select { |book| book_districts(book).include?("neukölln") }
+      @filtered_books = Book.all
     end
   end
 
