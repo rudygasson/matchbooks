68d8bad42aa646e5ec6a6d044c5a306669c612d9
Soma Zsoter <somazsoter@gmail.com>
2022-06-03 11:28:10 +0200

    set up meetings route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'meetings/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :copies, only: %i[index new create destroy]
   resources :books, only: %i[show]
+  resources :meetings, only: %i[index]
 end
