a6e681821eb4b58db4d65998d0ba898801ce6868
Soma Zsoter <somazsoter@gmail.com>
2022-06-03 14:31:22 +0200

    basic meeting page set up

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,5 @@
 class Meeting < ApplicationRecord
   enum :status, [ :pending, :cancelled, :confirmed ]
   belongs_to :location
+  has_many :handovers
 end
--- app/policies/meeting_policy.rb
+++ app/policies/meeting_policy.rb
@@ -2,8 +2,8 @@ class MeetingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      sql_query = "deliverer_id WHERE :query OR receiver_id WHERE :query"
-      scope.joins(:handover).where(sql_query, query: "#{user.id}")
+      # sql_query = "deliverer_id WHERE :query OR receiver_id WHERE :query"
+      scope.joins(:handovers).where(handovers: {deliverer_id: user.id}).or(scope.joins(:handovers).where(handovers: {receiver_id: user.id})).distinct
     end
   end
 
