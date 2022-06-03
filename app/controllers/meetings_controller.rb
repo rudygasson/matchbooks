ef74cb8e10d10b647375933409bd8bec427a38af
Soma Zsoter <somazsoter@gmail.com>
2022-06-03 13:45:14 +0200

    set up two approaches for meetings index (none are functional still)

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,4 +1,9 @@
 class MeetingsController < ApplicationController
   def index
+    # @handovers = Handover.where(deliverer_id: current_user.id) + Handover.where(receiver_id: current_user.id)
+    # @meetings = @handovers.map { |handover| handover.meeting }
+    # @meetings = @meetings.uniq
+    @meetings = policy_scope(Meeting)
+    @handovers
   end
 end
--- app/policies/meeting_policy.rb
+++ app/policies/meeting_policy.rb
@@ -1,8 +1,13 @@
 class MeetingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      sql_query = "deliverer_id WHERE :query OR receiver_id WHERE :query"
+      scope.joins(:handover).where(sql_query, query: "#{user.id}")
+    end
+  end
+
+  def index?
+    true
   end
 end
