fc11c806c21374f585132b731b09053d609bd3d4
Philipp Riedel <riedel.philipp@gmx.net>
2022-06-05 13:23:48 +0200

    add chat function with action cable to meeting show page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,18 @@
 class MessagesController < ApplicationController
   def create
     @meeting = Meeting.find(params[:meeting_id])
-    @chatroom = Chatroom.find_by(meeting_id: @meeting.id)
+    @chatroom = Chatroom.find_by(meeting_id: @meeting)
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
     authorize @message
     # raise
     if @message.save
-      redirect_to meeting_path(@meeting)
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "message", locals: { message: @message })
+      )
+      head :ok
     else
       render "meetings/show", status: :unprocessable_entity
     end
---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+// Connects to data-controller="chatroom-subscription"
+export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
+
+  // connect to chatroom channel
+  connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.#insertMessageAndScrollDown(data) }
+    )
+    // console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  // unsubscribe subscription when the user leaves the page -> the method gets
+  // triggered when the element of the controller disappears from the DOM
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+}
