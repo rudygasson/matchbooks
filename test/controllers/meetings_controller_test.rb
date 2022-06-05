bbb7f9040e5e58e42948e40dfdb563b24564326f
Christian Rudolf Hoppe <2715405+rudygasson@users.noreply.github.com>
2022-06-05 17:47:53 +0200

    remove test

--- test/controllers/meetings_controller_test.rb
+++ test/controllers/meetings_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class MeetingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get meetings_index_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
