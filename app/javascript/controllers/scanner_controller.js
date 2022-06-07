34482486c97309f595eaed0863e556778a8cfd98
Christian Rudolf Hoppe <2715405+rudygasson@users.noreply.github.com>
2022-06-07 17:16:39 +0200

    Scanner v0.1, only front

--- app/javascript/controllers/scanner_controller.js
+++ app/javascript/controllers/scanner_controller.js
@@ -1,50 +1,49 @@
 import { Controller } from "@hotwired/stimulus";
-import {
-  BrowserBarcodeReader,
-  BrowserCodeReader,
-  BarcodeFormat,
-} from "@zxing/library";
-import { DecodeHintType } from "@zxing/library";
+import Quagga from "@ericblade/quagga2";
 
 export default class extends Controller {
-
   connect() {
     console.log("Scanner controller connected");
-
-    let selectedDeviceId;
-    const hints = new Map();
-    const formats = [BarcodeFormat.EAN_13];
-    hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
-    const codeReader = new BrowserBarcodeReader();
-    console.log("ZXing code reader initialized");
-
-    codeReader.getVideoInputDevices()
-      .then((videoInputDevices) => {
-        selectedDeviceId = videoInputDevices[0].deviceId;
-
-        document.getElementById("startButton").addEventListener("click", () => {
-          codeReader
-            .decodeOnceFromVideoDevice(selectedDeviceId, "video")
-            .then((result) => {
-              console.log(result);
-              document.getElementById("result").textContent = result.text;
-            })
-            .catch((err) => {
-              console.error(err);
-              document.getElementById("result").textContent = err;
-            });
-          console.log(
-            `Started continous decode from camera with id ${selectedDeviceId}`
-          );
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#scan-video")
+        },
+        decoder: {
+          readers: ["ean_reader"],
+        },
+        locator: {
+          halfSample: true,
+          patchSize: "x-large"
+        }
+      },
+      function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.onDetected((data) => {
+          console.log(data.codeResult);
+          let code = data.codeResult.code
+          if (code.startsWith('978')) {
+            document.querySelector("#result").innerText = code;
+            Quagga.stop();
+          } else {
+            document.querySelector("#result").innerText = 'No ISBN';
+          }
         });
-
-        document.getElementById("resetButton").addEventListener("click", () => {
-          codeReader.reset();
-          console.log("Reset.");
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+      }
+    );
+    document.querySelector("#startButton").addEventListener("click", (e) => {
+      console.log("Start");
+      Quagga.start();
+    });
+    document.querySelector("#stopButton").addEventListener("click", (e) => {
+      console.log("Stop");
+      Quagga.stop();
+    });
   }
 }
