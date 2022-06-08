51cf38a0728b5299af4513c78291cd8c1aa98f24
Christian Rudolf Hoppe <2715405+rudygasson@users.noreply.github.com>
2022-06-08 17:10:18 +0200

    Add simple scanner and preparation code for confirmation modal

--- app/javascript/controllers/scanner_controller.js
+++ app/javascript/controllers/scanner_controller.js
@@ -3,27 +3,32 @@ import Quagga from "@ericblade/quagga2";
 import { Modal } from "bootstrap";
 
 export default class extends Controller {
-  connect() {
-    this.csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content")
-    console.log("Scanner controller connected");
-    console.log(this.csrfToken);
+  static targets = ["scanVideo", "foundIsbn"];
 
-    this.modal = new Modal(document.getElementById("exampleModal"));
-    this.modal.show();
+  connect() {
+    this.csrfToken = document
+      .querySelector('meta[name="csrf-token"]')
+      .getAttribute("content");
+    console.log("Scanner controller connected");
+  }
+
+  init() {
+    const isbnElement = this.foundIsbnTarget;
+    const confirmationModal = new Modal("#confirmationModal");
     Quagga.init(
       {
         inputStream: {
           name: "Live",
           type: "LiveStream",
-          target: document.querySelector("#scan-video")
+          target: this.scanVideoTarget,
         },
         decoder: {
           readers: ["ean_reader"],
         },
         locator: {
           halfSample: true,
-          patchSize: "x-large"
-        }
+          patchSize: "x-large",
+        },
       },
       function (err) {
         if (err) {
@@ -31,21 +36,24 @@ export default class extends Controller {
           return;
         }
         console.log("Initialization finished. Ready to start");
+        Quagga.start();
         Quagga.onDetected((data) => {
           console.log(data.codeResult);
-          let code = data.codeResult.code
-          if (code.startsWith('978')) {
-            document.querySelector("#result").innerText = code;
+          let code = data.codeResult.code;
+          if (code.startsWith("978")) {
             Quagga.stop();
+            document.querySelector("#book_isbn").value = code;
+            // confirm(code);
           } else {
-            document.querySelector("#result").innerText = 'No ISBN';
+            document.querySelector("#book_isbn").value = "No ISBN";
           }
         });
       }
     );
-    document.querySelector("#startButton").addEventListener("click", (e) => {
-      console.log("Start");
-      Quagga.start();
-    });
+  }
+
+  stop() {
+    Quagga.stop();
+    this.scanVideoTarget.innerHTML = "";
   }
 }
