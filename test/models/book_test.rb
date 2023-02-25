d7f5c7c4d2a31befc368d91f9a6024ce0056fb19
C. Rudy Hoppe <2715405+rudygasson@users.noreply.github.com>
2023-02-25 22:52:14 +0100

    Add first small tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should not save book without title" do
+    book = Book.new
+    assert_not book.save, "Saved Book without a title"
+  end
+
+  test "Should not save a book with an identical isbn" do
+    book = Book.new(
+      author: "Test",
+      title: "Test",
+      isbn: books(:turing).isbn,
+      year: 2012
+    )
+    assert_not book.save, "Saved Book with conflicting isbn"
+  end
 end
